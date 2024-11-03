28cc73432b30403ce1b3fa69b736b84d148c43cd
TheGirlThatLived <incredibleviolent@gmail.com>
2024-11-03 19:15:19 +0000

    modification to prevent mouse movement from being registred when mouse isn't locked
    A modification of your original script to prevent mouse movement from being registred when mouse isn't locked. Considering there are multiple "mousemove" event listeners, it distinguishes only the specific one it needs using toString().includes(.
    
    it makes positioning camera a lot easier if it's for recording

--- PointerLockChange disabler.js
+++ PointerLockChange disabler.js
@@ -4,19 +4,26 @@
 // @version      2024-11-03
 // @description  Disabling Event Listener, so pressing ESC once releases your mouse, and only pressing twice opens the menu
 // @author       Copilot
+// @run-at       document-start
 // @match        https://narrow.one/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
-    // Save original addEventListener function
+
     const originalAddEventListener = EventTarget.prototype.addEventListener;
     EventTarget.prototype.addEventListener = function(type, listener, options) {
-        if (type !== 'pointerlockchange') {
-            originalAddEventListener.call(this, type, listener, options);
+        if (type == 'pointerlockchange') return;
+        if (type == 'mousemove') {
+            if (listener.toString().includes('this.mouseIsOnDocument')) {
+                const originalListener = listener;
+                listener = function(event) {
+                    if (document.pointerLockElement) originalListener.call(this, event);
+                }
+            }
         }
+        originalAddEventListener.call(this, type, listener, options);
     };
 })();
