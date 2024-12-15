609f71676508b276f0f5daa94b4824e924c1d15d
The Girl that Lived <incredibleviolent@gmail.com>
2024-12-15 06:23:15 +0000

    Update CSS-Theme(ViolentGlass)[Auto-updates].js

--- Scripts/CSS-Theme(ViolentGlass)[Auto-updates].js
+++ Scripts/CSS-Theme(ViolentGlass)[Auto-updates].js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSS Theme (Violent Glass) [Auto-updates]
 // @namespace    http://tampermonkey.net/
-// @version      20241202_1840
+// @version      20241215_0722
 // @description  Guest 0187's personalized CSS edits.
 // @author       Incredible_Violent
 // @run-at       document-start
@@ -12,6 +12,7 @@
 // ==/UserScript==
 (function() {
     'use strict';
+    let stylesheetVersion = '';
     // Function to fetch and apply CSS
     function fetchAndApplyCSS() {
         GM_xmlhttpRequest({
@@ -19,6 +20,8 @@
             url: 'https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Styles/CSS-Theme(ViolentGlass).css',
             onload: function(response) {
                 if (response.status === 200) {
+                    const stylesheetVersionSanitize = response.responseText.split('\n');
+                    stylesheetVersion = stylesheetVersionSanitize[0].trim(); stylesheetVersion = stylesheetVersion.slice(2, -2).trim();
                     GM_addStyle(response.responseText);
                 } else {
                     console.error('Failed to fetch the CSS:', response.statusText);
@@ -33,7 +36,7 @@
     function fetchAndApplySVG() {
         GM_xmlhttpRequest({
             method: 'GET',
-            url: 'https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Assets/violentGlass.svg',
+            url: 'https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Assets/ViolentGlass.svg',
             onload: function(response) {
                 console.log('SVG fetch response:', response);
                 if (response.status === 200) {
@@ -41,22 +44,23 @@
                     const svgDoc = parser.parseFromString(response.responseText, 'image/svg+xml');
                     const svgElement = svgDoc.documentElement;
                     svgElement.style.position = 'fixed';
-                    svgElement.style.bottom = '10px';
-                    svgElement.style.right = '10px';
+                    svgElement.style.bottom = '3%';
+                    svgElement.style.right = '0%';
                     svgElement.style.zIndex = '9999';
                     svgElement.style.pointerEvents = 'none'; // To prevent interaction
                     document.body.appendChild(svgElement);
-                    svgElement.setAttribute('width', '100'); svgElement.setAttribute('height', '100');
+                    svgElement.setAttribute('width', '14%'); svgElement.setAttribute('height', '14%');
+                    svgElement.style.animation = `blinking 60s infinite`;
 
                     // Create and append version text
                     const versionText = document.createElement('div');
-                    versionText.textContent = 'Style/Updater Version: ' + GM_info.script.version;
+                    versionText.textContent = 'Version: ' + stylesheetVersion;
                     versionText.style.position = 'fixed';
-                    versionText.style.bottom = '3%';
+                    versionText.style.bottom = '10.5%';
                     versionText.style.right = '1%';
                     versionText.style.zIndex = '9999';
                     versionText.style.color = 'white';
-                    versionText.style.padding = '2px 5px';
+                    // versionText.style.padding = '2px 5px';
                     versionText.style.pointerEvents = 'none'; // Prevent interaction with the text
                     document.body.appendChild(versionText);
                 } else {
