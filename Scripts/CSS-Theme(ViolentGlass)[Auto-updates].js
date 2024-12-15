bf1406225e22ac4dd060f01c2c2c5f868aaa40ff
The Girl that Lived <incredibleviolent@gmail.com>
2024-12-15 07:49:06 +0000

    Update CSS-Theme(ViolentGlass)[Auto-updates].js

--- Scripts/CSS-Theme(ViolentGlass)[Auto-updates].js
+++ Scripts/CSS-Theme(ViolentGlass)[Auto-updates].js
@@ -6,7 +6,7 @@
 // @author       Incredible_Violent
 // @run-at       document-start
 // @match        https://narrow.one/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=narrow.one
+// @icon         https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Assets/ViolentGlass.svg
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // ==/UserScript==
