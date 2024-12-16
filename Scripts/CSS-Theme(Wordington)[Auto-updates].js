faa2a980a7e0eaad8897b323f5cc9ed3d8f36cc6
The Girl that Lived <incredibleviolent@gmail.com>
2024-12-16 06:31:10 +0000

    Update CSS-Theme(Wordington)[Auto-updates].js
    Auto-updating of script itself

--- Scripts/CSS-Theme(Wordington)[Auto-updates].js
+++ Scripts/CSS-Theme(Wordington)[Auto-updates].js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         CSS Theme (Wordington) [Auto-updates]
 // @namespace    http://tampermonkey.net/
-// @version      20241202_1840
+// @version      20241216_0730
 // @description  Guest 0187's sexualized CSS edits.
 // @author       Incredible_Violent
 // @run-at       document-start
 // @match        https://narrow.one/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=narrow.one
+// @downloadURL  https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Scripts/CSS-Theme(Wordington)%5BAuto-updates%5D.js
+// @updateURL    https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Scripts/CSS-Theme(Wordington)%5BAuto-updates%5D.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // ==/UserScript==
