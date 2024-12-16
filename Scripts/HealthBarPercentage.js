2aeb5db7ca4b39c7e88ac949044aff6eb62fbab1
The Girl that Lived <incredibleviolent@gmail.com>
2024-12-16 19:24:03 +0000

    Update HealthBarPercentage.js
    move the text% into heart-ui icon

--- Scripts/HealthBarPercentage.js
+++ Scripts/HealthBarPercentage.js
@@ -19,12 +19,12 @@ const healthBarObserver = new MutationObserver((mutationsList) => {
             const currentWidth = observedHealthBarPart.style.width;
 
             console.log('Width changed to:', currentWidth);
-            heathTextDisplayElement.textContent = currentWidth;
+            healthTextDisplayElement.textContent = currentWidth;
         }
     }
 });
 let observedHealthBarPart = null;
-let heathTextDisplayElement = document.createElement("div");
+let healthTextDisplayElement = document.createElement("div");
 
 const isHealthBar = (elem) => elem.classList.contains("health-ui-container");
 const gameWrapperObserver = new MutationObserver((mutationsList) => {
@@ -46,9 +46,9 @@ gameWrapperObserver.observe(document.getElementById("gameWrapper"), {
 
 function addHealthBar(element) {
     console.log("Added health bar", element);
-    element.append(heathTextDisplayElement);
+    element.append(healthTextDisplayElement);
 
-    observedHealthBarPart = document.getElementsByClassName("health-ui-bar clip")[0];
+    observedHealthBarPart = document.getElementsByClassName("health-ui-heart")[0];
     healthBarObserver.observe(observedHealthBarPart, {
         attributes: true,
         attributeFilter: ['style']
@@ -56,6 +56,6 @@ function addHealthBar(element) {
 }
 function removeHealthBar(element) {
     console.log("Removed health bar", element);
-    heathTextDisplayElement.remove();
+    healthTextDisplayElement.remove();
     healthBarObserver.disconnect();
 }
