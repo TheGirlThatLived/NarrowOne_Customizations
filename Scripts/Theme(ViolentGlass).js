ecc2ce8d998d83cbaadb21d4a5d1f1e687065ed5
The Girl that Lived <incredibleviolent@gmail.com>
2024-12-17 03:57:02 +0000

    Update Theme(ViolentGlass).js

--- Scripts/Theme(ViolentGlass).js
+++ Scripts/Theme(ViolentGlass).js
@@ -1,56 +1,95 @@
 // ==UserScript==
 // @name         Theme (Violent Glass)
 // @namespace    http://tampermonkey.net/
-// @version      20241216_2225
-// @description  Guest 0187's personalized UI edits.
+// @version      20241216_2230
+// @description  Guest 0187's personalized CSS edits loader.
 // @author       Incredible_Violent; Xeltalliv; Pratik; Systummm; Blazyst; Riptide; Razgriz; PING 18
 // @run-at       document-start
 // @match        https://narrow.one/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=narrow.one
-// @downloadURL  https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Scripts/Theme(ViolentGlass).js
-// @updateURL    https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Scripts/Theme(ViolentGlass).js
-// @grant        GM_xmlhttpRequest
-// @grant        GM_addStyle
+// @icon         https://TheGirlThatLived.github.io/NarrowOne_Customizations/Assets/ViolentGlass.svg
+// @downloadURL  https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Scripts/CSS-Theme(ViolentGlass)%5BAuto-updates%5D.js
+// @updateURL    https://raw.githubusercontent.com/TheGirlThatLived/NarrowOne_Customizations/refs/heads/main/Scripts/CSS-Theme(ViolentGlass)%5BAuto-updates%5D.js
 // @grant        unsafeWindow
+// @connect      TheGirlThatLived.github.io
 // ==/UserScript==
 (function() {
     'use strict';
-    function fetchAndApplyCSS() {
+
+    // Set of helper functions
+    const customStylesheets = [];
+    function finalizeApplyingConstructedStylesheets() {
+        window.addEventListener("load", function () {
+            document.adoptedStyleSheets = [...document.adoptedStyleSheets, ...customStylesheets];
+        });
+    }
+    function addScriptFromURL(url) {
+        const scriptElement = document.createElement('script');
+        scriptElement.src = url;
+        document.head.appendChild(scriptElement);
+    }
+    function addStyleFromURL(url) {
         const linkElement = document.createElement('link');
         linkElement.rel = 'stylesheet';
-        linkElement.href = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Styles/CSS-Theme(ViolentGlass).css';
+        linkElement.href = url;
         document.body.appendChild(linkElement);
+    }
+    function addStyleFromText(text) {
+        const styleElement = document.createElement('style');
+        styleElement.textContent = text;
+        document.body.appendChild(styleElement);
+    }
+    async function addConstructedStyleFromURL(url) {
+        const response = await fetch(url);
+        const text = await response.text();
+        const css = new CSSStyleSheet();
+        css.replaceSync(text);
+        customStylesheets.push(css);
+    }
+    function addConstructedStyleFromText(text) {
+        const css = new CSSStyleSheet();
+        css.replaceSync(text);
+        customStylesheets.push(css);
+    }
+    // End of helper functions
+
+
+    // Content
+    async function fetchAndApplyCSS() {
+        addStyleFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Styles/CSS-Theme(ViolentGlass).css');
+        //addConstructedStyleFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Styles/CSS-Theme(ViolentGlass).css');
     }
     function fetchAndApplyIMG() {
         const imgElement1 = document.createElement('img');
-        imgElement.src = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Assets/ViolentGlass.svg';
-        imgElement.style.position = 'fixed';
-        imgElement.style.bottom = '3%';
-        imgElement.style.right = '0%';
-        imgElement.style.zIndex = '9999';
-        imgElement.style.pointerEvents = 'none';
-        imgElement.setAttribute('width', '14%');
-        imgElement.setAttribute('height', '14%');
-        imgElement.style.animation = 'blinking 60s infinite';
+        imgElement1.classList.add('watermark');
+        imgElement1.src = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Assets/ViolentGlass.svg';
         document.body.appendChild(imgElement1);
+        addConstructedStyleFromText(`
+        .watermark {
+            position: fixed;
+            bottom: 3%;
+            right: 0%;
+            z-index: 9999;
+            pointer-events: none;
+            width: 14%;
+            height: 14%;
+            animation: blinking 60s infinite;
+        }
+        `);
         const imgElement2 = document.createElement('img');
-        imgElement.src = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Assets/crosshair(greenDot).svg';
+        imgElement2.src = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Assets/crosshair(greenDot).svg';
         document.body.appendChild(imgElement2);
         //document.querySelector('.crosshair-container');
         //document.querySelector('.crosshair-container').innerHTML = ``;
     }
     function fetchAndApplyJS() {
-        const scriptElement1 = document.createElement('script');
-        scriptElement.src = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/HealthBarPercentage.js';
-        document.head.appendChild(scriptElement1);
-        const scriptElement2 = document.createElement('script');
-        scriptElement.src = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/FlagsScoreUI.js';
-        document.head.appendChild(scriptElement2);
-        const scriptElement3 = document.createElement('script');
-        scriptElement.src = 'https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/CustomHealthBar.js';
-        document.head.appendChild(scriptElement3);
+        addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/HealthBarPercentage.js');
+        addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/FlagsScoreUI.js');
+        addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/CustomHealthBar.js');
     }
-    fetchAndApplyCSS();
-    fetchAndApplyJS();
-    fetchAndApplyIMG();
+
+    // Apply changes
+    fetchAndApplyCSS(); fetchAndApplyIMG(); fetchAndApplyJS();
+
+    // Mandatory end
+    finalizeApplyingConstructedStylesheets();
 })();
