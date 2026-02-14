ecfeb3a3edcfe3dbde0ab6773b4ea03c1ba2eb9f
The Girl that Lived <incredibleviolent@gmail.com>
2026-02-14 11:52:21 +0100

    Update Theme(ViolentGlass).js

--- Scripts/Theme(ViolentGlass).js
+++ Scripts/Theme(ViolentGlass).js
@@ -79,7 +79,7 @@
     }
     function fetchAndApplyJS() {
         addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/ImageSubstitution.js');
-        addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/CustomHealthBar.js');
+        addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/CustomHealthBar(ViolentGlass).js');
         addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/HealthBarPercentage.js');
         addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/FlagsScoreUI.js');
         addScriptFromURL('https://TheGirlThatLived.github.io/NarrowOne_Customizations/Scripts/DotCrosshair.js');
